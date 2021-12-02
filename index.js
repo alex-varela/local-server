a3007873514072d19d6fb7c88480b7129d72b90f
Carlos Rodríguez <52302464+carlos-ra@users.noreply.github.com>
2021-12-02 17:41:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var server = express();
+
+server.get('/.well-known/apple-app-site-association', function(request, response) {
+  response.sendFile(__dirname +  '/apple-app-site-association');
+});
+
+// server.get('/.well-known/assetlinks.json', function(request, response) {
+//   response.sendFile(__dirname +  '/assetlinks.json');
+// });
+
+server.listen(80);
