7200194f7bf7122fa2bd88c88ce46ab65eee4b26
Alex-Icoms <alex@icoms.co>
2022-11-09 12:10:43 -0600

    CHANGE PORT

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express = require('express');
+var express = require("express");
 var server = express();
 
-server.get('/.well-known/apple-app-site-association', function(request, response) {
-  response.sendFile(__dirname +  '/apple-app-site-association');
-});
+server.get(
+  "/.well-known/apple-app-site-association",
+  function (request, response) {
+    response.sendFile(__dirname + "/apple-app-site-association");
+  }
+);
 
 // server.get('/.well-known/assetlinks.json', function(request, response) {
 //   response.sendFile(__dirname +  '/assetlinks.json');
 // });
 
-server.listen(8888);
+server.listen(process.env.PORT || 8888);
